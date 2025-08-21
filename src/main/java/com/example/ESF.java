e80515713c1097802499da59066ac05ddafd92aa
FREINYES <104691904+FREINYES@users.noreply.github.com>
2025-08-21 10:05:19 +0300

    Initial commit

---
+++ src/main/java/com/example/ESF.java
@@ -0,0 +1,36 @@
+package com.example;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.*;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public class ESF implements ModInitializer {
+
+    public static final String MOD_ID = "esf";
+
+    // Вкладка предметів мода
+    public static final RegistryKey<ItemGroup> ESF_GROUP = RegistryKey.of(RegistryKeys.ITEM_GROUP,
+            Identifier.of(MOD_ID, "group"));
+
+    // Сам веник
+    public static final Item VENIK = register("venik", new Venik(new Item.Settings().maxCount(1)));
+
+    @Override
+    public void onInitialize() {
+        // Реєстрація вкладки
+        Registry.register(Registries.ITEM_GROUP, ESF_GROUP, FabricItemGroup.builder()
+                .displayName(Text.translatable("itemGroup." + MOD_ID + ".group"))
+                .icon(() -> new ItemStack(VENIK))
+                .entries((context, entries) -> {entries.add(VENIK);}).build());
+    }
+
+    private static Item register(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(MOD_ID, name), item);
+    }
+}
---
+++ src/main/java/com/example/Venik.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import net.minecraft.block.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.util.ActionResult;
+import net.minecraft.world.World;
+import net.minecraft.block.Block;
+
+public class Venik extends Item {
+
+    public Venik(Settings settings) {
+        super(settings);
+    }
+
+    // ПКМ по блоку → чистимо сніг
+    @Override
+    public ActionResult useOnBlock(ItemUsageContext context) {
+        World world = context.getWorld();
+        Block block = world.getBlockState(context.getBlockPos()).getBlock();
+
+        if (!world.isClient) {
+            if (block == Blocks.SNOW || block == Blocks.SNOW_BLOCK) {
+                world.breakBlock(context.getBlockPos(), false); // чистимо без дропа
+                return ActionResult.SUCCESS;
+            }
+        }
+        return super.useOnBlock(context);
+    }
+}
